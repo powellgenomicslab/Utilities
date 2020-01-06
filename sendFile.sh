9171df7d7d82c580114a2e0ed59ea847c6a4df6b
Anne Senabouth <a.senabouth@garvan.org.au>
2020-01-06 13:16:26 +1100

    Bah

--- sendFile.sh
+++ sendFile.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+# Get current directory
+SCRIPT_DIR=$( dirname "$0" )
+
 # Include parse_yaml function
-. parse_yaml.sh
+. ${SCRIPT_DIR}/parse_yaml.sh
 
 # User needs to supply the following
 recipient="$1"
@@ -11,4 +15,4 @@ files="$4"
 # Read in yaml_file with username and yaml key
 eval $(parse_yaml ~/FileSenderConfig.yaml "login_")
 
-python ./filesender.py -u "$login_user" -a "$login_api" -s "$subject" -m "$message" -r "$recipient" "$files"
+python ${SCRIPT_DIR}/filesender.py -u "$login_user" -a "$login_api" -s "$subject" -m "$message" -r "$recipient" "$files"
